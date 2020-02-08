0ad903718bc277aced165ba1dec1548ac1690fa5
MSI <wydrasz3@gmail.com>
2020-02-08 11:50:40 +0100

    singleton prototype

---
+++ src/main/java/pl/sda/Main.java
@@ -0,0 +1,24 @@
+package pl.sda;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("my-beans.xml");
+
+        Message myMessage1 = (Message) context.getBean("myMessage1");
+        Message myMessage2 = (Message) context.getBean("myMessage2");
+        Message myMessage3 = (Message) context.getBean("myMessage3");
+
+        Message myMessageSingleton = (Message) context.getBean("myMessage2");
+        Message myMessagePrototype = (Message) context.getBean("myMessage3");
+
+        System.out.println(myMessage1.getText());
+        System.out.println(myMessage2.getText());
+        System.out.println(myMessage3.getText());
+
+
+    }
+}
